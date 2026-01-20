66e5b1dc2a7ca9a2df96650f571c23ddd04306da
AJ Hannah <527ahannah@frhsd.com>
2026-01-20 18:16:08 +0000

    I FINALLY DID ACCESSIBILITY

--- scripts.js
+++ scripts.js
@@ -1,41 +1,28 @@
+// remy helped with this part otherwise i would have been cooked
+
 try {
 const accessibilityToggle = document.getElementById("accessibilityToggle");
 
 function generateName() {
   
 }
-document.getElementById('generateBtn').addEventListener('click', generateName);
-accessibleGenerateBtn.addEventListener("click", generateName);
+document.getElementById("generateBtn").addEventListener("click", generateName);
+document.getElementById("accessibleGenerateBtn").addEventListener("click", generateName);
 } catch (error) {
   alert(error)
 }
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   alert("domloaded")
-//   alert(accessibilityToggle);
-//   if (!accessibilityToggle) {
-//     console.error("Accessibility toggle not found");
-//     return;
-//   }
-
-//   document.querySelector("#toggleBox").addEventListener("click", () => {
-//     // document.body.classList.toggle("accessible");
-//     alert("Accessibility mode toggled");
-
-//     //input.checked true or false
-//   });
-// });
 window.onload = function() {
-  alert("domloaded")
-  alert(accessibilityToggle);
+  // alert("domloaded")
+  // alert(accessibilityToggle);
   if (!accessibilityToggle) {
     console.error("Accessibility toggle not found");
     return;
   }
 
   document.querySelector("#toggleBox").addEventListener("click", () => {
-    // document.body.classList.toggle("accessible");
-    alert("Accessibility mode toggled");
+    document.body.classList.toggle("accessible");
+    // alert("Accessibility mode toggled");
 
     //input.checked true or false
   });
